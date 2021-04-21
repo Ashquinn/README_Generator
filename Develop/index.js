9aee9817b8d516cbc4afc5e59bb4213740cdb8f3
Ashquinn Gordon <ashquinngordon@gmail.com>
2021-04-21 19:15:29 -0400

    outline added to structure README file

---
+++ Develop/index.js
@@ -0,0 +1,27 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+
+
+//Questions to ask 
+//What is the title of your project?
+//Please include your project description.
+//How can users install this application?
+//Please provide instructions on how to use your application.
+//How can users contribute to this application?
+//What are the tests associated with your project?
+//Enter your Github username.
+//Enter your email address.
+//Which licencse woould your like for your project?
