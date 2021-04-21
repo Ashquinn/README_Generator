a23354f2154ee6244d725f1fa94f83e9f44359c2
Ashquinn Gordon <ashquinngordon@gmail.com>
2021-04-21 19:21:01 -0400

    questions added to the questions array

--- Develop/index.js
+++ Develop/index.js
@@ -2,7 +2,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is the title of your project?', 'Please include a description of your project', 'How can users install this application?', 'Please provide a list of instructions on how to use your application', 'How can users contribute to your application?', 'What are the tests associated with your project?', 'Enter your Github username', 'Enter your email address', 'Which license would you like for your project?'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -12,16 +12,3 @@ function init() {}
 
 // Function call to initialize app
 init();
-
-
-
-//Questions to ask 
-//What is the title of your project?
-//Please include your project description.
-//How can users install this application?
-//Please provide instructions on how to use your application.
-//How can users contribute to this application?
-//What are the tests associated with your project?
-//Enter your Github username.
-//Enter your email address.
-//Which licencse woould your like for your project?
